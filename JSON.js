523c11e8aba13755a585609bb345c153aef1ceaf
Bhavishanth Suresh <bhavishanth@gmail.com>
2023-03-17 19:18:23 +0530

    Add files via upload

---
+++ JSON.js
@@ -0,0 +1,40 @@
+var resume = [ { 
+    "Heading": "Name and Age",
+    "Name": "bhavishanth",
+    "Age": "24"
+},
+{
+    "Heading": "Qualification",
+    "Degree": "B.E Manufacturing Engineering",
+    "College": "CEG, Anna University",
+    "Year": "2020",
+    "University": "Anna University"
+},
+{
+    "Heading": "Work Experience",
+    "Years": "2 years 3 months",
+    "Company": "Cognizant"
+},
+{ 
+    "Heading": "Skills",
+    "Tech Stack": "Javascript, HTML"
+},
+{
+    "Heading": "Academics(CGPA/Percentage)",
+    "12th Grade": "96.83",
+    "10th Grade": "10/10 CGPA"
+}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
